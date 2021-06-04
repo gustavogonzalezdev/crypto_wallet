d502ce75c778659f1c7bfe211b8c33b551150345
Gustavo Gonzalez <52711264+gustavogonzalezdev@users.noreply.github.com>
2021-06-04 16:11:18 -0300

    update

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -1,6 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<void> signIn(String email, String password) async {}
+Future<bool> signIn(String email, String password) async {
+  try {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    return true;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
 
 Future<bool> register(String email, String password) async {
   try {
@@ -18,4 +28,40 @@ Future<bool> register(String email, String password) async {
     print(e.toString());
     return false;
   }
+}
+
+Future<bool> addCoin(String id, String amount) async {
+  try {
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    var value = double.parse(amount);
+    DocumentReference documentReference = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .collection('Coin')
+        .doc(id);
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(documentReference);
+      if (!snapshot.exists) {
+        documentReference.set({'Amount': value});
+        return true;
+      }
+      double newAmount = snapshot.data()['Amount'] + value;
+      transaction.update(documentReference, {'Amount': newAmount});
+      return true;
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<bool> removeCoin(String id) async {
+  String uid = FirebaseAuth.instance.currentUser.uid;
+  FirebaseFirestore.instance
+      .collection('Users')
+      .doc(uid)
+      .collection('Coins')
+      .doc(id)
+      .delete();
+  return true;
 }
--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -28,33 +28,41 @@ class _AuthenticationState extends State<Authentication> {
           children: [
             Container(
               width: MediaQuery.of(context).size.width / 1.3,
-              child: TextFormField(),
-              controller: _emailField,
-              decoration: InputDecoration(
-                hintText: "something@email.com",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            TextFormField(
-              controller: _passwordField,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "password",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Password",
-                labelStyle: TextStyle(
-                  color: Colors.white,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "something@email.com",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
             Container(
               width: MediaQuery.of(context).size.width / 1.4,
               height: 45,
@@ -66,14 +74,41 @@ class _AuthenticationState extends State<Authentication> {
                 onPressed: () async {
                   bool shouldNavigate =
                       await register(_emailField.text, _passwordField.text);
-                  if(shouldNavigate) {
-                    //Navigate
+                  if (shouldNavigate) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeView(),
+                      ),
+                    );
                   }
                 },
                 child: Text("Register"),
               ),
             ),
-            Container(),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                  onPressed: () async {
+                    bool shouldNavigate =
+                        await signIn(_emailField.text, _passwordField.text);
+                    if (shouldNavigate) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeView(),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Login")),
+            ),
           ],
         ),
       ),
